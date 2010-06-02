02277f716f1502b7adf6b939c57bd50459c16147
Simon Denier <simon.denier@gmail.com>
2010-06-03 01:46:28 +0200

    Declare version 0.9b2

--- src/valmo/geco/core/Geco.java
+++ src/valmo/geco/core/Geco.java
@@ -46,7 +46,7 @@ import com.apple.eawt.ApplicationEvent;
  */
 public class Geco {
 	
-	public static final String VERSION = "0.9b1"; 
+	public static final String VERSION = "0.9b2"; 
 	
 	private class RuntimeStage {
 		private Stage stage;
