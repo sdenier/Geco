5ac58a46d4dbff86e68d9fb38a0418efce299bbb
Simon Denier <simon.denier@gmail.com>
2011-05-26 22:16:36 +0200

    Add ConfigPanel interface

---
+++ src/net/geco/ui/framework/ConfigPanel.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2011 Simon Denier
+ * Released under the MIT License (see LICENSE file)
+ */
+package net.geco.ui.framework;
+
+
+/**
+ * @author Simon Denier
+ * @since May 22, 2011
+ *
+ */
+public interface ConfigPanel {
+	
+	public String getLabel();
+
+}
