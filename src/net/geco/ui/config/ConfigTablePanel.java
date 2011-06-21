fdcf4f6623f19b82f97a98bee01abab67a71ca2d
pfigger <pfigger@geco.net>
2011-06-22 00:47:19 +0200

    Added OrangeExtensions (released under permissive BSD license) to allow compilation under Windows Some minor changes - two typos in net\geco\ui\messages_de.properties - some comments in various files

--- src/net/geco/ui/config/ConfigTablePanel.java
+++ src/net/geco/ui/config/ConfigTablePanel.java
@@ -61,6 +61,7 @@ public class ConfigTablePanel<T> extends JPanel {
 				ListSelectionModel.SINGLE_SELECTION);
 		table.setPreferredScrollableViewportSize(table.getPreferredSize());
 		JScrollPane jsp = new JScrollPane(table);
+		//TODO PWA: Find a more elegant way to do this
 		//jsp.setPreferredSize(new Dimension(75, 275));		
 		jsp.setPreferredSize(new Dimension(250, 275));
 		add(jsp, BorderLayout.CENTER);
