3477f88a3605357159754e9380d48acd7c690de1
Simon Denier <simon.denier@gmail.com>
2012-09-25 00:48:17 +0200

    Remove unused import

--- src/net/geco/Geco.java
+++ src/net/geco/Geco.java
@@ -14,10 +14,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
-import com.sun.tools.hat.internal.parser.Reader;
-
-import sun.tools.tree.FinallyStatement;
-
 import net.geco.app.AppBuilder;
 import net.geco.app.GecoStageLaunch;
 import net.geco.basics.Announcer;
