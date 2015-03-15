5a51259b7a50e3a513a45227eee026c440547523
Simon Denier <simon.denier@gmail.com>
2015-03-15 22:29:39 +0100

    reapply JarClassLoader patch 9303161: select native jar entry according to os.arch property
    os.arch = x86_64 will match an entry like 64bits/lib.dll
    os.arch = x86 will match an entry like 32bits/lib.dll
    
    os.arch is ignored on Mac as jnilib/dylib are multi-arch

--- src/com/jdotsoft/jarloader/JarClassLoader.java
+++ src/com/jdotsoft/jarloader/JarClassLoader.java
@@ -526,6 +526,7 @@ public class JarClassLoader extends ClassLoader {
      * @return Native library entry.
      */
     private JarEntryInfo findJarNativeEntry(String sLib) {
+    	String archPattern = getArchMatchingPattern();
         String sName = System.mapLibraryName(sLib);
         for (JarFileInfo jarFileInfo : lstJarFile) {
             JarFile jarFile = jarFileInfo.jarFile;
@@ -541,7 +542,7 @@ public class JarClassLoader extends ClassLoader {
                 //   - in the path: abc/Native.dll/xyz/my.dll <-- do not load this one!
                 //   - in the partial name: abc/aNative.dll   <-- do not load this one!
                 String[] token = sEntry.split("/"); // the last token is library name
-                if (token.length > 0 && token[token.length - 1].equals(sName)) {
+                if (sEntry.contains(archPattern) && token.length > 0 && token[token.length - 1].equals(sName)) {
                     logInfo(LogArea.NATIVE, "Loading native library '%s' found as '%s' in JAR %s",
                             sLib, sEntry, jarFileInfo.simpleName);
                     return new JarEntryInfo(jarFileInfo, je);
@@ -550,6 +551,14 @@ public class JarClassLoader extends ClassLoader {
         }
         return null;
     } // findJarNativeEntry()
+
+    private String getArchMatchingPattern() {
+    	if (System.getProperty("os.name").startsWith("Mac")) {
+    		return "";
+    	} else {
+        	return System.getProperty("os.arch").contains("64") ? "64" : "32";
+    	}
+    }
 
     /**
      * Loads class from a JAR and searches for all jar-in-jar.
